5b7af13f6602ee60902003fa8b3ed1c01d1ea621
Sebastian <sebanicosp@gmail.com>
2019-03-26 22:39:14 -0300

    Add: card border fix

--- lib/main.dart
+++ lib/main.dart
@@ -30,6 +30,7 @@ class _HomeAppState extends State<HomeApp> {
     // TODO: implement build
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Color.fromRGBO(31, 178, 101, 100),
         title: Text("Cabañas Pichidangui"),
         actions: <Widget>[
           IconButton(
@@ -53,12 +54,21 @@ class _HomeAppState extends State<HomeApp> {
     return Container(
       width: MediaQuery.of(context).size.width * 0.95,
       child: Card(
-        borderOnForeground: true,
-        child: Column(
-          children: <Widget>[_dibujarInterior()],
+        semanticContainer: true,
+        clipBehavior: Clip.antiAliasWithSaveLayer,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30.0)
         ),
-      ),
-    );
+        borderOnForeground: true,
+        elevation: 5,
+        margin: EdgeInsets.all(10),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(30.0),
+          child: _dibujarInterior(),
+        )
+        )
+      );
+    
   }
 
   Widget _dibujarInterior() {
@@ -66,20 +76,42 @@ class _HomeAppState extends State<HomeApp> {
       children: <Widget>[
         Image.network(
           "https://www.ecestaticos.com/imagestatic/clipping/136/649/136649f8e2860ea92b2ed8811039dd77/imagen-sin-titulo.jpg?mtime=1543236555",
-          fit: BoxFit.cover,
+          fit: BoxFit.fill,
         ),
         Positioned(
-          top: 150,
-          left: 50,
+          bottom: 25,
+          left: 20,
           child: Text(
-            "Hola",
+            "Cabaña delantera",
             style: TextStyle(
               fontSize: 25.0,
               color: Colors.white,
 
             ),
           ),
-        )
+        ),
+        Positioned(
+          top: 15.0,
+          right: 20.0,
+          child: Text(
+            'Mie 05 - Vie 07',
+            style: TextStyle(
+              fontSize: 19.0,
+              color: Colors.white
+            ),
+            ),
+        ),
+        Positioned(
+          top: 35.0,
+          right: 35.0,
+          child: Text(
+            'Febrero',
+            style: TextStyle(
+              fontSize: 24.0,
+              color: Colors.white
+            ),
+            ),
+        ),
       ],
     );
   }
